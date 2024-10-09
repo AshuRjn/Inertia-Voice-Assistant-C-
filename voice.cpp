c56c436f9f3701b92c7239688132adf63b5e25f4
Ashutosh Ranjan <148480368+AshuRjn@users.noreply.github.com>
2024-10-09 15:15:32 +0530

    Add files via upload
    Inertia is a personal virtual assistant developed in C++. It leverages speech synthesis and recognition capabilities to provide an interactive experience, enabling users to perform various tasks through voice commands. The assistant can greet users, respond to inquiries, manage applications, and navigate the web, making it a handy tool for enhancing productivity.

---
+++ voice.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctime>
+
+using namespace std;
+main()
+{
+    cout<<"\t\t\t<===================== WELCOME ===================================>"<<endl;
+    cout<<"\t\t\t<===================== I'M A VIRTUAL ASSISTANT =========================>"<<endl;
+    cout<<"\t\t\t<===================== MY NAME IS shivi ==============================>"<<endl;
+    cout<<"\t\t\t<===================== I'M HERE TO HELP YOU ============================>"<<endl<<endl;
+
+    cout<< "Good Morning sir"<<endl;
+        string phrase = " welcome,  i'm virtual assistant,  my name is shivi,  i'm here to help you ";
+                string command = "espeak \"" + phrase + "\"";
+                const char *charCommand = command.c_str();
+                system(charCommand);
+        
+        password:
+        string password;
+        cout<<"======================="<<endl;
+        cout<<"| ENTER YOUR PASSWORD |"<<endl;
+        cout<<"======================="<<endl<<endl;
+         phrase = " enter your password";
+                 command = "espeak \"" + phrase + "\"";
+                charCommand = command.c_str();
+                system(charCommand);
+        
+    cin>>password;
+
+    if(password=="ashu")
+    {
+      cout<<"<-----welcome ashutosh----->\n"
+          <<"<-----how can i help you------>\n";
+
+      phrase = "welcome ashu, how can i help you,";
+                 command = "espeak \"" + phrase + "\"";
+                charCommand = command.c_str();
+                system(charCommand);
+    }
+    else
+    {
+        cout<<"incorrect password\n";
+        cout<<"try again \n";
+        phrase = "incorrect password , try again,";
+                 command = "espeak \"" + phrase + "\"";
+                charCommand = command.c_str();
+                system(charCommand);
+                goto password;
+
+    }            
+      {
+               string z;
+               cout<<"=> ";
+               cin>>z; 
+               if(z=="google"){
+                system("start https:\\www.google.com");
+                phrase = "opeaning google,";
+                 command = "espeak \"" + phrase + "\"";
+                charCommand = command.c_str();
+                system(charCommand);
+
+               }
+
+    }
+          
+}
